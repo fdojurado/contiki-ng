7ff14e8c975fdebac27f00d2f268e58f465597d9
Fernando Jurado <ffjla@dtu.dk>
2022-03-29 18:45:25 +0200

    fit iot lab hello world run script working

--- examples/benchmarks/fit-iot-lab/run.py
+++ examples/benchmarks/fit-iot-lab/run.py
@@ -1,4 +1,24 @@
 import os
-os.system("cd ../../hello-world")
-os.system("ARCH_PATH=../../../arch make TARGET=iotlab BOARD=m3 savetarget")
-os.system("ARCH_PATH=../../../arch make")
+from os import chdir as cd
+from tkinter import COMMAND
+
+ARCH_PATH = "../../../arch"
+APP_DIR = "../../hello-world"
+
+TARGET = "iotlab"
+BOARD = "m3"
+
+cd("../../hello-world")
+
+COMMAND = "ARCH_PATH="+ARCH_PATH+" make TARGET=" + \
+    TARGET+" BOARD="+BOARD+" savetarget"
+
+print(COMMAND)
+
+COMMAND2 = "ARCH_PATH="+ARCH_PATH+" make "
+
+print(COMMAND2)
+
+
+os.system(COMMAND)
+os.system(COMMAND2)
