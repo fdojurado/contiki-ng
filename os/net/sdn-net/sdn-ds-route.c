8f096c986d690063b6696a8c5989476488a11482
Fernando Jurado <ffjla@dtu.dk>
2022-04-01 13:36:00 +0200

    sdn-sd-route.c: change address printing, starting from the 1 address to 0 address.

--- os/net/sdn-net/sdn-ds-route.c
+++ os/net/sdn-net/sdn-ds-route.c
@@ -256,9 +256,9 @@ sdn_ds_route_lookup(const linkaddr_t *addr)
     if (found_route != NULL)
     {
         PRINTF("Found route: %d.%d",
-               addr->u8[0], addr->u8[1]);
+               addr->u8[1], addr->u8[0]);
         PRINTF(" via %d.%d\n",
-               sdn_ds_route_nexthop(found_route)->u8[0], sdn_ds_route_nexthop(found_route)->u8[1]);
+               sdn_ds_route_nexthop(found_route)->u8[1], sdn_ds_route_nexthop(found_route)->u8[0]);
     }
     else
     {
