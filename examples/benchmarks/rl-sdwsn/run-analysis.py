304103b042a2ecabc9c65430070641f13b0786af
Fernando Jurado <ffjla@dtu.dk>
2022-06-15 16:02:25 +0200

    run-analysis curve fit added

--- examples/benchmarks/rl-sdwsn/run-analysis.py
+++ examples/benchmarks/rl-sdwsn/run-analysis.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as pl
 from database import *
 import math
+from numpy import arange
+from scipy.optimize import curve_fit
 
 
 #######################################################
@@ -263,7 +265,8 @@ def plot_against_sf_size(df):
     y = stats['mean']
 
     axs[0, 0].plot(x, y, 'b-o', markersize=data_marker_size)
-    axs[0, 0].fill_between(x, stats['ci95_hi'], stats['ci95_lo'], color='b', alpha=.1)
+    axs[0, 0].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
 
     # Second plot: Power vs. slotframe size
     axs[0, 1].set_title('Network avg. power vs. SF size',
@@ -281,7 +284,8 @@ def plot_against_sf_size(df):
     y = stats['mean']
 
     axs[0, 1].plot(x, y, 'b-o', markersize=data_marker_size)
-    axs[0, 1].fill_between(x, stats['ci95_hi'], stats['ci95_lo'], color='b', alpha=.1)
+    axs[0, 1].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
 
     # Third plot: Delay vs. slotframe size
     axs[1, 0].set_title('Network avg. delay vs. SF size',
@@ -299,7 +303,8 @@ def plot_against_sf_size(df):
     y = stats['mean']
 
     axs[1, 0].plot(x, y, 'b-o', markersize=data_marker_size)
-    axs[1, 0].fill_between(x, stats['ci95_hi'], stats['ci95_lo'], color='b', alpha=.1)
+    axs[1, 0].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
 
     # Fourth plot: PDR vs. slotframe size
     axs[1, 1].set_title('Network avg. PDR vs. SF size',
@@ -317,13 +322,188 @@ def plot_against_sf_size(df):
     y = stats['mean']
 
     axs[1, 1].plot(x, y, 'b-o', markersize=data_marker_size)
-    axs[1, 1].fill_between(x, stats['ci95_hi'], stats['ci95_lo'], color='b', alpha=.1)
+    axs[1, 1].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
     axs[1, 1].set_yticks(np.arange(0.8, 1.05, 0.05))
-
 
     # Save plot
 
     pl.savefig('alpha{},beta{},delta{},last_ts{},sf_size.pdf'.format(
+        alpha_weight, beta_weight, delta_weight, last_ts), bbox_inches='tight')
+    pl.close()
+
+
+#######################################################
+# power_objective function
+def power_objective(x, a, b, c, d):
+    return a * x + b * x**2 + c * x**3 + d
+
+
+def power_equation(x, a=-0.00283, b=0.00004, c=-0.00000, d=0.26416):
+    return a * x + b * x**2 + c * x**3 + d
+
+
+def delay_equation(x, a=-0.00077, b=0.00008, c=-0.00000, d=0.06175):
+    return a * x + b * x**2 + c * x**3 + d
+
+
+def pdr_equation(x, a=0.00146, b=-0.00003, c=0.00000, d=0.97998):
+    return a * x + b * x**2 + c * x**3 + d
+#######################################################
+# power_objective function
+
+
+def linear_objective(x, a, b):
+    return a * x + b
+#######################################################
+
+
+def plot_fit_curves(df):
+    title_font_size = 8
+    x_axis_font_size = 8
+    y_axis_font_size = 8
+    ticks_font_size = 7
+    data_marker_size = 1.5
+    legend_font_size = 6
+    title_fontweight = 'bold'
+    axis_labels_fontstyle = 'italic'
+
+    fig, axs = pl.subplots(2, 2, layout='constrained')
+    alpha_weight = df['alpha'].iloc[0]
+    beta_weight = df['beta'].iloc[0]
+    delta_weight = df['delta'].iloc[0]
+    last_ts = df['last_ts_in_schedule'].iloc[0]
+    fig.suptitle(r'$\alpha={},\beta={},\delta={},last~ts={}$'.format(
+        alpha_weight, beta_weight, delta_weight, last_ts), fontsize=title_font_size)
+
+    # First plot the power and the fitted curved
+    axs[0, 1].set_title('Network power normalized vs. SF size',
+                        fontsize=title_font_size, fontweight=title_fontweight)
+    axs[0, 1].set_xlabel('SF size', fontsize=x_axis_font_size,
+                         fontstyle=axis_labels_fontstyle)
+    axs[0, 1].set_ylabel(
+        'Power [mW]', fontsize=y_axis_font_size, fontstyle=axis_labels_fontstyle)
+    axs[0, 1].tick_params(axis='both', which='major',
+                          labelsize=ticks_font_size)
+    # Confidence interval for all sf size
+    stats = calculate_confidence_interval(
+        df, 'current_sf_len', 'power_normalized')
+
+    x = stats['current_sf_len']
+    y = stats['mean']
+
+    axs[0, 1].plot(x, y, 'b-o', markersize=data_marker_size)
+    axs[0, 1].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
+    # Fit the curve
+    # popt, _ = curve_fit(power_objective, x, y)
+    # # summarize the parameter values
+    # power_a, power_b, power_c, power_d = popt
+    # print('power = %.5f * x + %.5f * x^2 + %.5f * x^3 + %.5f' %
+    #       (power_a, power_b, power_c, power_d))
+    x_line = arange(min(x), max(x), 1)
+    # calculate the output for the range
+    y_line = power_equation(x_line)
+    # y_line = power_objective(x_line, power_a, power_b, power_c, power_d)
+    axs[0, 1].plot(x_line, y_line, 'r-o', markersize=data_marker_size)
+
+    # Fit the plot for the delay
+    axs[1, 0].set_title('Network delay normalized vs. SF size',
+                        fontsize=title_font_size, fontweight=title_fontweight)
+    axs[1, 0].set_xlabel('SF size', fontsize=x_axis_font_size,
+                         fontstyle=axis_labels_fontstyle)
+    axs[1, 0].set_ylabel(
+        'Delay [ms]', fontsize=y_axis_font_size, fontstyle=axis_labels_fontstyle)
+    axs[1, 0].tick_params(axis='both', which='major',
+                          labelsize=ticks_font_size)
+    # Confidence interval for all sf size
+    stats = calculate_confidence_interval(
+        df, 'current_sf_len', 'delay_normalized')
+
+    x = stats['current_sf_len']
+    y = stats['mean']
+
+    axs[1, 0].plot(x, y, 'b-o', markersize=data_marker_size)
+    axs[1, 0].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
+    # Fit the curve
+    # popt, _ = curve_fit(power_objective, x, y)
+    # # summarize the parameter values
+    # delay_a, delay_b, delay_c, delay_d = popt
+    # print('delay= %.5f * x + %.5f * x^2 + %.5f * x^3 + %.5f' %
+    #       (delay_a, delay_b, delay_c, delay_d))
+    x_line = arange(min(x), max(x), 1)
+    # calculate the output for the range
+    y_line = delay_equation(x_line)
+    # y_line = power_objective(x_line, delay_a, delay_b, delay_c, delay_d)
+    axs[1, 0].plot(x_line, y_line, 'r-o', markersize=data_marker_size)
+
+    # Fit the plot for the PDR
+    axs[1, 1].set_title('Network PDR normalized vs. SF size',
+                        fontsize=title_font_size, fontweight=title_fontweight)
+    axs[1, 1].set_xlabel('SF size', fontsize=x_axis_font_size,
+                         fontstyle=axis_labels_fontstyle)
+    axs[1, 1].set_ylabel(
+        'PDR', fontsize=y_axis_font_size, fontstyle=axis_labels_fontstyle)
+    axs[1, 1].tick_params(axis='both', which='major',
+                          labelsize=ticks_font_size)
+    # Confidence interval for all sf size
+    stats = calculate_confidence_interval(
+        df, 'current_sf_len', 'pdr_wam')
+
+    x = stats['current_sf_len']
+    y = stats['mean']
+
+    axs[1, 1].plot(x, y, 'b-o', markersize=data_marker_size)
+    axs[1, 1].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
+    # Fit the curve
+    # popt, _ = curve_fit(power_objective, x, y)
+    # # summarize the parameter values
+    # pdr_a, pdr_b, pdr_c, pdr_d = popt
+    # print('PDR = %.5f * x + %.5f * x^2 + %.5f * x^3 + %.5f' %
+    #       (pdr_a, pdr_b, pdr_c, pdr_d))
+    x_line = arange(min(x), max(x), 1)
+    # calculate the output for the range
+    y_line = pdr_equation(x_line)
+    # y_line = power_objective(x_line, pdr_a, pdr_b, pdr_c, pdr_d)
+    axs[1, 1].plot(x_line, y_line, 'r-o', markersize=data_marker_size)
+
+    # Plot the reward and the estimated reward
+    axs[0, 0].set_title('Network reward vs. SF size',
+                        fontsize=title_font_size, fontweight=title_fontweight)
+    axs[0, 0].set_xlabel('SF size', fontsize=x_axis_font_size,
+                         fontstyle=axis_labels_fontstyle)
+    axs[0, 0].set_ylabel(
+        'Reward', fontsize=y_axis_font_size, fontstyle=axis_labels_fontstyle)
+    axs[0, 0].tick_params(axis='both', which='major',
+                          labelsize=ticks_font_size)
+    # Confidence interval for all sf size
+    stats = calculate_confidence_interval(
+        df, 'current_sf_len', 'reward')
+
+    x = stats['current_sf_len']
+    y = stats['mean']
+
+    axs[0, 0].plot(x, y, 'b-o', markersize=data_marker_size)
+    axs[0, 0].fill_between(x, stats['ci95_hi'],
+                           stats['ci95_lo'], color='b', alpha=.1)
+
+    print(f'reward first element {y[0]}')
+    # Calculate the reward using the regression data
+    # x = arange(20, 60, 1)
+    power_estimated = power_equation(x)
+
+    delay_estimated = delay_equation(x)
+
+    pdr_estimated = pdr_equation(x)
+
+    reward = -1*(alpha_weight*power_estimated+beta_weight *
+                 delay_estimated-delta_weight*pdr_estimated)
+
+    axs[0, 0].plot(x, reward, 'r-o', markersize=data_marker_size)
+
+    pl.savefig('alpha{},beta{},delta{},last_ts{},fitted.pdf'.format(
         alpha_weight, beta_weight, delta_weight, last_ts), bbox_inches='tight')
     pl.close()
 
@@ -351,6 +531,9 @@ def main():
 
     # Plot chars against the SF size
     plot_against_sf_size(df)
+
+    # Fit curves for power, delay, PDR.
+    plot_fit_curves(df)
 
 
 #######################################################
