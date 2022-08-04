7a69b9df3a77781363e4a2ca502ffc265c49bd5a
Fernando Jurado <ffjla@dtu.dk>
2022-08-04 11:06:28 +0200

    FIT IoT target, now, tx at -30 dbm to form a multihop network.

--- examples/sdn-tsch/tsch-sdn-node/sdn-node.c
+++ examples/sdn-tsch/tsch-sdn-node/sdn-node.c
@@ -141,7 +141,7 @@ PROCESS_THREAD(sdn_node_process, ev, data)
     // Possible values for M3 radio 3, 2.8, 2.3, 1.8, 1.3, 0.7, 0.0, -1,
     // -2, -3, -4, -5, -7, -9, -12, -17
     // see phy.h for correct value to use
-    NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, PHY_POWER_m7dBm);
+    NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, PHY_POWER_m30dBm);
 #endif /* CONTIKI_TARGET_IOTLAB */
     /*  */
     sdn_power_measurement_init();
