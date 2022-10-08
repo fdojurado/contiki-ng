c0a30316d43a4d774e012a1b96f086b2b5325870
Fernando Jurado <ffjla@dtu.dk>
2022-10-08 17:54:58 +0200

    move elise example to the sdn-tsch folder

--- examples/sdn-tsch/run-cooja.py
+++ examples/sdn-tsch/run-cooja.py
@@ -9,20 +9,23 @@ from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 # get the path of this example
 SELF_PATH = os.path.dirname(os.path.abspath(__file__))
 # move three levels up
-CONTIKI_PATH = os.path.dirname(os.path.dirname(os.path.dirname(SELF_PATH)))
+CONTIKI_PATH = os.path.dirname(os.path.dirname(SELF_PATH))
 
-cooja_jar = os.path.normpath(os.path.join(CONTIKI_PATH, "tools", "cooja", "dist", "cooja.jar"))
+COOJA_PATH = os.path.normpath(os.path.join(CONTIKI_PATH, "tools", "cooja"))
 cooja_input = 'cooja.csc'
 cooja_output = 'COOJA.testlog'
+cooja_log = 'COOJA.testlog'
 
 #######################################################
 # Run a child process and get its output
+
 
 def run_subprocess(args, input_string):
     retcode = -1
     stdoutdata = ''
     try:
-        proc = Popen(args, stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True, universal_newlines=True)
+        proc = Popen(args, stdout=PIPE, stderr=STDOUT, stdin=PIPE,
+                     shell=True, universal_newlines=True)
         (stdoutdata, stderrdata) = proc.communicate(input_string)
         if not stdoutdata:
             stdoutdata = '\n'
@@ -42,6 +45,7 @@ def run_subprocess(args, input_string):
 #############################################################
 # Run a single instance of Cooja on a given simulation script
 
+
 def execute_test(cooja_file):
     # cleanup
     try:
@@ -52,8 +56,17 @@ def execute_test(cooja_file):
         print("Cannot remove previous Cooja output:", ex)
         return False
 
+    try:
+        os.remove(cooja_log)
+    except FileNotFoundError as ex:
+        pass
+    except PermissionError as ex:
+        print("Cannot remove previous Cooja log:", ex)
+        return False
+
     filename = os.path.join(SELF_PATH, cooja_file)
-    args = " ".join(["java -Djava.awt.headless=true -jar ", cooja_jar, "-nogui=" + filename, "-contiki=" + CONTIKI_PATH])
+    args = " ".join(["cd", COOJA_PATH, "&&", "./gradlew run --args='-nogui=" +
+                    filename, "-contiki=" + CONTIKI_PATH+" -logdir="+SELF_PATH+" -logname=COOJA"+"'"])
     sys.stdout.write("  Running Cooja, args={}\n".format(args))
 
     (retcode, output) = run_subprocess(args, '')
@@ -83,10 +96,8 @@ def execute_test(cooja_file):
 #######################################################
 # Run the application
 
+
 def main():
-    if not os.access(cooja_jar, os.R_OK):
-        print('The file "{}" does not exist, did you build Cooja?'.format(cooja_jar))
-        exit(-1)
 
     input_file = cooja_input
     if len(sys.argv) > 1:
@@ -102,6 +113,7 @@ def main():
         exit(-1)
 
 #######################################################
+
 
 if __name__ == '__main__':
     main()
